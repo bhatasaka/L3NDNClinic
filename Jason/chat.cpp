750403068d3099d44b04a803e16a4f336c6d3967
Jason Stauffer <jstauffer94@gmail.com>
2019-11-05 13:34:12 -0700

    Added NDN code, added documentation. NDN code is currently commented out because it is not yet functioning

--- Jason/chat.cpp
+++ Jason/chat.cpp
@@ -2,14 +2,132 @@
 
 // Header file - requreid for terminal input and output
 #include <iostream>
-// This include requires the file to be saved in the correct location
-// #include <ndn-cxx/face.hpp>
+#include <ndn-cxx/face.hpp>
+#include <ndn-cxx/security/key-chain.hpp>
+#include <ndn-cxx/security/signing-helpers.hpp>
 
-// placeholder: global variable list (not recommended to use this)
+// placeholder: global variable list
 
 // Function declaration
 std::string getUsername();
 std::string getChatPartner();
+
+// This class is taken from the NDN tutorial "Trivial Consumer", and modified by Jason Stauffer
+// Enclosing code in ndn simplifies coding (can also use `using namespace ndn`)
+namespace ndn {
+// Additional nested namespaces should be used to prevent/limit name conflicts
+namespace examples {
+
+class Consumer
+{
+public:
+  void
+  run()
+  {
+	// Specify the name for the interest
+    Name interestName("/ndn/l3clinic/chat");
+    // Append NDN version number to the interested
+    interestName.appendVersion();
+	
+	// Print the interest name to the terminal
+	std::cout << "Creating interest: " << interestName << std::endl;
+	
+	// Create an interested with the interest name specified
+    Interest interest(interestName);
+    
+    // Set interest parameters
+    interest.setCanBePrefix(false);
+    interest.setMustBeFresh(true);
+    interest.setInterestLifetime(6_s); // The default is 4 seconds
+
+    std::cout << "Sending Interest " << interest << std::endl;
+    m_face.expressInterest(interest,
+                           bind(&Consumer::onData, this,  _1, _2),
+                           bind(&Consumer::onNack, this, _1, _2),
+                           bind(&Consumer::onTimeout, this, _1));
+
+    // processEvents will block until the requested data is received or a timeout occurs
+    m_face.processEvents();
+  }
+
+private:
+  void
+  onData(const Interest&, const Data& data) const
+  {
+    std::cout << "Received Data " << data << std::endl;
+  }
+
+  void
+  onNack(const Interest&, const lp::Nack& nack) const
+  {
+    std::cout << "Received Nack with reason " << nack.getReason() << std::endl;
+  }
+
+  void
+  onTimeout(const Interest& interest) const
+  {
+    std::cout << "Timeout for " << interest << std::endl;
+  }
+
+private:
+  Face m_face;
+};
+
+// This class is taken from NDN examples "Trivial Producer" and modified by Jason Stauffer
+
+class Producer
+{
+public:
+  void
+  run()
+  {
+    m_face.setInterestFilter("/ndn/l3clinic/chat",
+                             bind(&Producer::onInterest, this, _1, _2),
+                             nullptr, // RegisterPrefixSuccessCallback is optional
+                             bind(&Producer::onRegisterFailed, this, _1, _2));
+    m_face.processEvents();
+  }
+
+private:
+  void
+  onInterest(const InterestFilter&, const Interest& interest)
+  {
+    std::cout << ">> I: " << interest << std::endl;
+
+    static const std::string content("Hello, world!");
+
+    // Create Data packet
+    auto data = make_shared<Data>(interest.getName());
+    data->setFreshnessPeriod(10_s);
+    data->setContent(reinterpret_cast<const uint8_t*>(content.data()), content.size());
+
+    // Sign Data packet with default identity
+    m_keyChain.sign(*data);
+    // m_keyChain.sign(*data, signingByIdentity(<identityName>));
+    // m_keyChain.sign(*data, signingByKey(<keyName>));
+    // m_keyChain.sign(*data, signingByCertificate(<certName>));
+    // m_keyChain.sign(*data, signingWithSha256());
+
+    // Return Data packet to the requester
+    std::cout << "<< D: " << *data << std::endl;
+    m_face.put(*data);
+  }
+
+  void
+  onRegisterFailed(const Name& prefix, const std::string& reason)
+  {
+    std::cerr << "ERROR: Failed to register prefix '" << prefix
+              << "' with the local forwarder (" << reason << ")" << std::endl;
+    m_face.shutdown();
+  }
+
+private:
+  Face m_face;
+  KeyChain m_keyChain;
+};
+
+} // namespace examples
+} // namespace ndn
 
 // Main Function
 int main() {
@@ -31,7 +149,7 @@ int main() {
 	while ( true ) {
 		// Check for message from other user
 		messageRecieved = true;
-		recievedMessage = "Response";
+		recievedMessage = "This is an arbitrary response";
 		
 		if ( messageRecieved ) {
 			std::cout << otherUser << ": " << recievedMessage << std::endl;
@@ -40,7 +158,7 @@ int main() {
 		
 		// Send a response
 		std::cout << username << ": ";
-		std::cin >> sendMessage;
+		std::getline (std::cin, sendMessage);
 	}
 	
 	return 0;
@@ -50,7 +168,15 @@ int main() {
 std::string getChatPartner() {
 	std::string username;
 	
+	// Start by checking if any active searches for a chat
+	// ndn::examples::Consumer consumer;
+	// consumer.run();
+	
 	username = "Bob";
+	
+	// if no active chat requests
+	// ndn::examples::Producer producer;
+	// producer.run();
 	
 	return username;
 }
@@ -60,20 +186,23 @@ std::string getUsername() {
 	std::string username;
 	char confirm;
 	
+	// Create name label to jump back in code to the point where the username is chosen
 	createName:
 	
 	// recieve username
 	std::cout << "Please enter your username: ";
-	std::cin >> username;
+	std::getline(std::cin, username);
 	
 	// Confirm username
 	std::cout << "Your username is: " << username << std::endl;
 	
+	// Confirm name label to jump back in code to the point where the username is confirmed
 	confirmName:
 	
 	std::cout << "Confirm username? (type y/n): ";
 	std::cin >> confirm;
 	
+	// If username confirmed, return it. If username is denied, go back to recreate it. If unknown input, warn the user and try again
 	if ( confirm == 'y' ) {
 		return username;
 	}
